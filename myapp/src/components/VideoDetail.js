dbd821efefb0b66e62493eeec430248f84369725
Heggy Castaneda <heggy_castaneda@gap.com>
2019-07-01 18:45:01 -0700

    feat: add VideoDetail component to show video detail info

--- myapp/src/components/VideoDetail.js
+++ myapp/src/components/VideoDetail.js
@@ -5,11 +5,22 @@ const VideoDetail = ({ video }) => {
   // since when first visit video value is set to null to avoid this 
   //  we add Loading message
   if (!video) {
-    return <div>Loading...</div>
+    return <div>Loading...</div>;
   }
 
   // props.video ===> video destructuring es6
-  return <div>{video.snippet.title}</div>
+  return (
+    <div>
+      {/* ui segment is card box style */}
+      <div className="ui segment">
+        <h4 className="ui header">
+          {video.snippet.title}
+        </h4>
+        <p>{video.snippet.description}</p>
+      </div>
+      
+    </div>
+  );
 }
 
 export default VideoDetail;
